f37febee9ac66f6b0e851d0f3e3def661e5b21f6
maartenverheul <maarten.verheul@student.hu.nl>
2020-11-12 14:44:34 +0100

    Add Cuelist

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,20 @@ export default class App extends React.Component {
   changeStyleMode(e) {
     document.documentElement.setAttribute("data-theme", e.target.value);
   }
+  getCueList() {
+    let result = [];
+    for (let i = 0; i < 10; i++) {
+      result.push(<tr>
+        <td></td>
+        <td>{i}</td>
+        <td>Careless Whisper</td>
+        <td>00:00.00</td>
+        <td>00:22.80</td>
+        <td>00:00.00</td>
+      </tr>);
+    }
+    return result;
+  }
 
   render() {
     const { mode } = this.state;
@@ -27,7 +41,23 @@ export default class App extends React.Component {
           P
         </div>
         <div className="cuelist">
-          C
+          <div className="cuelist-container">
+            <table cellSpacing="0">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th className="w-auto">#</th>
+                  <th>Cue</th>
+                  <th className="w-auto p-2">Pre wait</th>
+                  <th className="w-auto p-2">Action</th>
+                  <th className="w-auto p-2">Post wait</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.getCueList()}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     );
