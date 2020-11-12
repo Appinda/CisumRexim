aeb6b469eb4c45a14d429d72a6c69971d83510f7
maartenverheul <maarten.verheul@student.hu.nl>
2020-11-12 14:14:21 +0100

    Add darkmode option

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'dark',
+    };
+  }
+
+  changeStyleMode(e) {
+    document.documentElement.setAttribute("data-theme", e.target.value);
+  }
+
+  render() {
+    const { mode } = this.state;
+    return (
+      <div className={`App stylemode-${mode}`}>
+        <div className="header">
+          <select className="combo" onChange={(e) => this.changeStyleMode(e)}>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+          </select>
+        </div>
+        <div className="playback">
+          P
+        </div>
+        <div className="cuelist">
+          C
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
